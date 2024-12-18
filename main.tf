d2d20e6c215c2fe1485176a9cde96b568cd06081
Ofri3 <libermanofri@gmail.com>
2024-12-18 14:25:11 +0200

    main.tf - add helm provider

--- main.tf
+++ main.tf
@@ -11,7 +11,6 @@ locals {
   sliced_url   = replace(local.original_url, "https://", "")
 }
 
-
 terraform {
   required_providers {
     aws = {
@@ -31,16 +30,17 @@ provider "aws" {
   region = var.region
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
-
-# module "s3_backup" {
-#   source             = "./modules/s3"
-#   backup_bucket_name = var.backup_bucket_name
-# }
 
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
@@ -65,30 +65,13 @@ module "s3_bucket" {
       }
 
       expiration = {
-        days = 30 # Expire objects after 30 days
+        days = 30 # Delete objects after 30 days
       }
     }
   ]
+
   tags = {
     aws_backup = true
-  }
-}
-
-module "velero_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  role_name             = "velero"
-  attach_velero_policy  = true
-  velero_s3_bucket_arns = ["arn:aws:s3:::eks-velero-backup-bucket"]
-
-  oidc_providers = {
-    ex = {
-      provider_arn               = "arn:aws:iam::023196572641:oidc-provider/${local.sliced_url}"
-      namespace_service_accounts = ["velero:velero"]
-    }
-  }
-
-  tags = {
-    Createdby = "OFRI"
   }
 }
 
@@ -104,7 +87,7 @@ module "velero" {
     version       = "8.0.0"  # Specify the Velero chart version
     chart         = "velero"
     force_update  = true
-    wait          = true
+    wait          = false
     recreate_pods = false
     deploy        = 1
   }
@@ -112,14 +95,40 @@ module "velero" {
   # Reference a values file stored in your repository
   values = [
     templatefile("${path.module}/values/velero.yaml", {
-      region               = var.region
-      bucket_name          = var.backup_bucket_name
-      cluster_oidc_issuer  = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+      region               = "us-east-2"
+      bucket_name          = "eks-velero-backup-bucket"
+      velero_role_arn      = module.velero_irsa_role.iam_role_arn
     })
   ]
   # Dynamic Helm chart settings
   set = [
+    {
+      name  = "serviceAccount.server.create"
+      value = "true"
+    },
+    {
+      name  = "serviceAccount.server.name"
+      value = "velero"
+    }
   ]
+}
+
+module "velero_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name             = "velero"
+  attach_velero_policy  = true
+  velero_s3_bucket_arns = ["arn:aws:s3:::eks-velero-backup-bucket"]
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = "arn:aws:iam::023196572641:oidc-provider/${local.sliced_url}"
+      namespace_service_accounts = ["velero-ofri:velero"]
+    }
+  }
+
+  tags = {
+    Createdby = "OFRI"
+  }
 }
 
 module "aws_backup" {
@@ -127,15 +136,13 @@ module "aws_backup" {
   backup_vault_name = "ofri-backup-project"
   backup_plan_name  = "ofri-backup-plan"
   backup_rule_name  = "ofri-backup-rule"
-  schedule          = "cron(0 * * * ? *)" # Hourly Backup
+  schedule          = "cron(0 12 * * ? *)"
   delete_after      = 97
   cold_storage_after = 7
   selection_name    = "ofri-backup-selection"
   tag_key           = "aws_backup"
   tag_value         = "true"
   role_name         = "aws-backup-role"
-  role_policy_name  = "AWSBackupServiceRolePolicyForS3Backup"
-  role_policy_arn   = "arn:aws:iam::aws:policy/AWSBackupServiceRolePolicyForS3Backup"
 }
 
 
